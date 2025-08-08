7b62b3c3faf6b6c2631f5bb1f99b4de368aea45b
Keisya31 <keinala23@gmail.com>
2025-08-09 01:03:13 +0700

    Menambahkan tailwind

---
+++ resources/views/kkn.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KKN Ecoprinting</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+</head>
+<body>
+    <header class="bg-orange-300 text-black p-4">
+        <div class="container mx-auto text-center">
+            <h1>ECOPRINTING ADALAH</h1>
+        </div>
+    </header>    
+
+    
+</body>
+</html>
